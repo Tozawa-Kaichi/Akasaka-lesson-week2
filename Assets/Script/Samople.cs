8a5d1241ef5b161e38afe6aebee2e5632dce7af2
戸澤海智 <st37071556@icloud.com>
2022-05-13 16:09:22 +0900

    ＄文字列補完

--- Assets/Script/Samople.cs
+++ Assets/Script/Samople.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Samople : MonoBehaviour
 {
+    int x = 0;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello World",this);
+        
+        var x = 10;
+        var y = 20;
+
+        // + ‰‰Zq‚Å•¶š—ñŒ‹‡
+        Debug.Log("x=" + x + ", y=" + y);
+
+        // $ ‹L†‚É‚æ‚é•âŠÔ•¶š—ñ
+        Debug.Log($"x={x}, y={y}");
     }
 
     // Update is called once per frame
