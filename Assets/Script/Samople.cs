119fb4355e68ccfeefde1e27b668850749cc6256
戸澤海智 <st37071556@icloud.com>
2022-05-13 17:08:09 +0900

    https://gist.github.com/LeonAkasaka/b26c55ff53523e86a12ad26a1972bc1a 課題有

--- Assets/Script/Samople.cs
+++ Assets/Script/Samople.cs
@@ -33,19 +33,44 @@ public class Samople : MonoBehaviour
     {
         // ■配列型変数宣言
         // 要素型[] 変数名;
-        int[] iAry;
+        //int[] iAry;
 
         // ■配列の生成
         // new 要素型[要素数];
-        iAry = new int[3]; // 配列生成と変数への保存
+        //iAry = new int[3]; // 配列生成と変数への保存
 
         // 配列要素へのアクセス
-        iAry[0] = 10;
-        iAry[1] = 20;
-        iAry[2] = 30;
+        //iAry[0] = 10;
+        //iAry[1] = 20;
+        //iAry[2] = 30;
 
-        Debug.Log($"iAry[0]={iAry[0]}");
-        Debug.Log($"iAry[1]={iAry[1]}");
-        Debug.Log($"iAry[2]={iAry[2]}");
+        //Debug.Log($"iAry[0]={iAry[0]}");
+        //Debug.Log($"iAry[1]={iAry[1]}");
+        //Debug.Log($"iAry[2]={iAry[2]}");
+
+        // ■配列初期化子
+        // 配列も変数と文法的には同じ宣言と同時に初期化できる。
+        // 配列を作成すると同時に要素を設定できる。
+        // new 要素型[要素数] { 要素1, 要素2, 要素3, ... }
+        // var iAry = new int[3] { 10, 20, 30 };
+
+        // 初期値から要素数と型を推論できる場合
+        // new の後の型と要素数も省略可能。
+        var iAry = new[] { 10, 20, 30, 40, 50 };
+
+        // Lengh プロパティ、配列の要素数を取得する
+        Debug.Log($"iAry.Length={iAry.Length}");
+
+        // for 文による配列アクセス
+        for (var i = 0; i < iAry.Length; i++)
+        {
+            Debug.Log($"iAry[{i}]={iAry[i]}");
+        }
+
+        // foreach 文による配列処理
+        foreach (var e in iAry)
+        {
+            Debug.Log($"e={e}");
+        }
     }
 }
